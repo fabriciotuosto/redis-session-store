f164e117d7f669b490d5554f668025626f324019
Fabricio Tuosto <fabricio@olga.localdomain>
2011-06-19 12:57:04 -0300

    Transform ObjectSerialization to MsgPack using messagePack Libray

--- src/main/java/org/redis/session/store/RedisSessionStore.java
+++ src/main/java/org/redis/session/store/RedisSessionStore.java
@@ -8,6 +8,7 @@ import java.io.ObjectOutputStream;
 import java.net.UnknownHostException;
 import java.util.Arrays;
 import java.util.Set;
+import java.util.logging.Level;
 
 import org.apache.catalina.Session;
 import org.apache.catalina.Store;
@@ -15,175 +16,151 @@ import org.apache.catalina.session.StoreBase;
 
 import redis.clients.jedis.Jedis;
 import java.util.logging.Logger;
+import org.msgpack.MessagePack;
 
-public class RedisSessionStore extends StoreBase implements Store{
+public class RedisSessionStore extends StoreBase implements Store {
 
-	private static final Logger LOGGER = Logger.getLogger("RedisSessionStore");
-	
-	private volatile Jedis jedis;
-	private String host;
-	private int port;
-	private int database;
-	private String password;
-	/**
-	 * @throws IOException
-	 */
-	public RedisSessionStore() throws IOException {
+    private static final Logger LOGGER = Logger.getLogger("RedisSessionStore");
+    private volatile Jedis jedis;
+    private String host;
+    private int port;
+    private int database;
+    private String password;
 
-	}
-	
-	@Override
-	public void clear() throws IOException {
-		LOGGER.info("Clearing Session database");
-		getJedis().flushDB();
-	}
+    /**
+     * @throws IOException
+     */
+    public RedisSessionStore() throws IOException {
+    }
 
-	@Override
-	public int getSize() throws IOException {
-		LOGGER.info("Returning size of Session database");
-		return getJedis().dbSize().intValue();
-	}
+    @Override
+    public void clear() throws IOException {
+        LOGGER.info("Clearing Session database");
+        getJedis().flushDB();
+    }
 
-	@Override
-	public String[] keys() throws IOException {
-		String[] keys = null;
-		Set<String> keysStored = getJedis().keys("*");
-		keys = new String[keysStored.size()];
-		keysStored.toArray(keys);
-		LOGGER.info("Retrieving keys: "+Arrays.asList(keys));
-		return keys;
-	}
+    @Override
+    public int getSize() throws IOException {
+        LOGGER.info("Returning size of Session database");
+        return getJedis().dbSize().intValue();
+    }
 
-	@Override
-	public Session load(String key) throws ClassNotFoundException, IOException {
-		Session session = null;
-		if(isNotEmpty(key)){
-			session = fromByteArray(getJedis().get(key.getBytes()));
-		}
-		session = session != null ? session : createEmptySession();
-		LOGGER.info("Loading key: "+key+" retrieving session: "+session);
-		return session;
-	}
+    @Override
+    public String[] keys() throws IOException {
+        String[] keys = null;
+        Set<String> keysStored = getJedis().keys("*");
+        keys = new String[keysStored.size()];
+        keysStored.toArray(keys);
+        LOGGER.log(Level.INFO, "Retrieving keys: {0}", Arrays.asList(keys));
+        return keys;
+    }
 
-	private boolean isNotEmpty(String key) {
-		return key != null && "".equals(key.trim());
-	}
+    @Override
+    public Session load(String key) throws ClassNotFoundException, IOException {
+        Session session = null;
+        if (isNotEmpty(key)) {
+            session = fromByteArray(getJedis().get(key.getBytes()));
+        }
+        session = session != null ? session : createEmptySession();
+        LOGGER.log(Level.INFO, "Loading key: {0} retrieving session: {1}", new Object[]{key, session});
+        return session;
+    }
 
-	private Session createEmptySession() throws IOException {
-		Session session = getManager().createEmptySession();
-		return session;
-	}
+    private boolean isNotEmpty(String key) {
+        return key != null && "".equals(key.trim());
+    }
 
-	@Override
-	public void remove(String key) throws IOException {
-		LOGGER.info("Removing session with key :"+String.valueOf(key));
-		if(isNotEmpty(key)){
-			getJedis().del(key.getBytes());
-		}
-	}
+    private Session createEmptySession() throws IOException {
+        Session session = getManager().createEmptySession();
+        return session;
+    }
 
-	@Override
-	public void save(Session session) throws IOException {
-		LOGGER.info("Saving session :"+session);
-		if(session != null)
-			getJedis().set(session.getId().getBytes(), toByteArray(session));
-		
-	}
+    @Override
+    public void remove(String key) throws IOException {
+        LOGGER.log(Level.INFO, "Removing session with key :{0}", String.valueOf(key));
+        if (isNotEmpty(key)) {
+            getJedis().del(key.getBytes());
+        }
+    }
 
-	private Jedis getJedis() throws UnknownHostException, IOException{
-		Jedis local = jedis;
-		if(local == null || !local.isConnected() || !"PONG".equals(local.ping())){
-			synchronized (this) {
-				local = jedis;
-				if(local == null || !local.isConnected() || !"PONG".equals(local.ping())){
-					jedis = local = createClient();
-				}
-			}
-		}
-		return local;
-	}
-	
-	private Jedis createClient() throws UnknownHostException, IOException {
-		LOGGER.info("Creating new jedis client");
-		Jedis client = new Jedis(host, port);
-		client.select(database);
-		if(isNotEmpty(password)){
-			client.auth(password);
-		}
-		client.connect();
-		return client;
-	}
+    @Override
+    public void save(Session session) throws IOException {
+        LOGGER.log(Level.INFO, "Saving session :{0}", session);
+        if (session != null) {
+            getJedis().set(session.getId().getBytes(), toByteArray(session));
+        }
 
-	private byte[] toByteArray(Session session) throws IOException {
-		byte[] result = null;
-		ByteArrayOutputStream output = null;
-		ObjectOutputStream objectOutput = null;
-		try{
-			output = new ByteArrayOutputStream();
-			objectOutput = new ObjectOutputStream(output);
-			objectOutput.writeObject(session);
-			result = output.toByteArray();
-		}finally{
-			if(output != null)
-				output.close();
-			if(objectOutput != null)
-				objectOutput.close();
-		}
-		return result;
-	}
-	
-	/**
-	 * @param bytes
-	 * @return
-	 * @throws IOException
-	 * @throws ClassNotFoundException
-	 */
-	private Session fromByteArray(byte[] bytes) throws IOException, ClassNotFoundException {
-		Session session = null;
-		ByteArrayInputStream stream = null;
-		ObjectInputStream objectStream = null;
-		try{
-			stream = new ByteArrayInputStream(bytes);
-			objectStream = new ObjectInputStream(stream);
-			session = (Session) objectStream.readObject();
-		}finally{
-			if(stream != null)
-				stream.close();
-			if(objectStream != null)
-				objectStream.close();
-		}
-		return session;
-	}
+    }
 
-	public String getHost() {
-		return host;
-	}
+    private Jedis getJedis() throws UnknownHostException, IOException {
+        Jedis local = jedis;
+        if (local == null || !local.isConnected() || !"PONG".equals(local.ping())) {
+            synchronized (this) {
+                local = jedis;
+                if (local == null || !local.isConnected() || !"PONG".equals(local.ping())) {
+                    jedis = local = createClient();
+                }
+            }
+        }
+        return local;
+    }
 
-	public void setHost(String host) {
-		this.host = host;
-	}
+    private Jedis createClient() throws UnknownHostException, IOException {
+        LOGGER.info("Creating new jedis client");
+        Jedis client = new Jedis(host, port);
+        client.select(database);
+        if (isNotEmpty(password)) {
+            client.auth(password);
+        }
+        client.connect();
+        return client;
+    }
 
-	public int getPort() {
-		return port;
-	}
+    private byte[] toByteArray(Session session) throws IOException {
+        byte[] result = MessagePack.pack(session);
+        return result;
+    }
 
-	public void setPort(int port) {
-		this.port = port;
-	}
+    /**
+     * @param bytes
+     * @return
+     * @throws IOException
+     * @throws ClassNotFoundException
+     */
+    private Session fromByteArray(byte[] bytes) throws IOException, ClassNotFoundException {
+        Session session = MessagePack.unpack(bytes, Session.class);
+        return session;
+    }
 
-	public int getDatabase() {
-		return database;
-	}
+    public String getHost() {
+        return host;
+    }
 
-	public void setDatabase(int database) {
-		this.database = database;
-	}
+    public void setHost(String host) {
+        this.host = host;
+    }
 
-	public String getPassword() {
-		return password;
-	}
+    public int getPort() {
+        return port;
+    }
 
-	public void setPassword(String password) {
-		this.password = password;
-	}
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    public int getDatabase() {
+        return database;
+    }
+
+    public void setDatabase(int database) {
+        this.database = database;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
